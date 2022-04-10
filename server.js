3c508525fde44c043dd4146d164bdd0d68c3fb78
Jonathan-Shih <jonathan25shih@gmail.com>
2022-04-10 17:24:37 -0400

    a9 mongo sp22

--- server.js
+++ server.js
@@ -3,22 +3,20 @@ import cors from "cors";
 import mongoose from "mongoose";
 import helloController from "./controllers/hello-controller.js";
 import userController from "./controllers/user-controller.js";
-import tuitController from "./controllers/tuits-controller.js";
+import tuitsController from "./controllers/tuits-controller.js";
 
-const CONNECTION_STRING =
-  process.env.DB_CONNECTION_STRING || "mongodb://localhost:27017/webdev";
-mongoose.connect(CONNECTION_STRING);
-
+mongoose.connect(
+  process.env.DB_CONNECTION_STRING || "mongodb://localhost:27017/webdev"
+);
 const app = express();
-
-app.listen(process.env.PORT || 4000);
-app.use(express.json());
 app.use(cors());
 
+app.use(express.json());
 helloController(app);
 userController(app);
-tuitController(app);
-
+tuitsController(app);
 app.get("/", (req, res) => {
   res.send("Welcome to Full Stack Development!");
 });
+
+app.listen(process.env.PORT || 4000);
--- server.ts
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-const CONNECTION_STRING =
-  process.env.DB_CONNECTION_STRING || "mongodb://localhost:27017/webdev";
-mongoose.connect(CONNECTION_STRING);
