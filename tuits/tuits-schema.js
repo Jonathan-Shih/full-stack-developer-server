48d304da5af3b92f2b3cb4cdcab4342689a75e86
Jonathan-Shih <jonathan25shih@gmail.com>
2022-04-12 02:08:42 -0400

    Update tuits-schema.js

--- tuits/tuits-schema.js
+++ tuits/tuits-schema.js
@@ -22,7 +22,7 @@ const schema = mongoose.Schema(
     },
     time: {
       type: Date,
-      default: Date.now.toUTCString(),
+      default: Date.now.toUTCString,
     },
     title: String,
     tuit: String,
