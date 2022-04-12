150818d27ec445581e427b69147cf832a4d692b0
Jonathan-Shih <jonathan25shih@gmail.com>
2022-04-12 01:42:08 -0400

    Update tuits-schema.js

--- tuits/tuits-schema.js
+++ tuits/tuits-schema.js
@@ -1,10 +1,49 @@
 import mongoose from "mongoose";
 const schema = mongoose.Schema(
   {
-    tuit: String,
-    likes: Number,
+    topic: String,
     postedBy: {
-      username: String,
+      userName: {
+        type: String,
+        default: Jon,
+      },
+    },
+    liked: {
+      type: Boolean,
+      default: false,
+    },
+    verified: {
+      type: Boolean,
+      default: true,
+    },
+    handle: {
+      type: String,
+      default: "JonShih",
+    },
+    time: {
+      type: Date,
+      default: Date.now,
+    },
+    title: String,
+    tuit: String,
+    profileImage: {
+      type: String,
+      default: "../../tuiter/assets/rat-jam.gif",
+    },
+    linkedImage: String,
+    stats: {
+      comments: {
+        type: Number,
+        default: 0,
+      },
+      retuits: {
+        type: Number,
+        default: 0,
+      },
+      likes: {
+        type: Number,
+        default: 0,
+      },
     },
   },
   { collection: "tuits" }
